31a86c913cfce9638e19a5685f50e2006988c85e
Mikael <mikael20informatica@gmail.com>
2021-09-18 18:49:44 -0300

    refatorando barra de carregamento.. mudando para a parte superior

--- lib/presenter/views/startPage.dart
+++ lib/presenter/views/startPage.dart
@@ -37,93 +37,96 @@ class _StartPageState extends ModularState<StartPage, HomeController> {
     return SafeArea(
         child: Scaffold(
             body: Stack(
-              children: [
-                Stack(
-                  children: [
-                    Container(
-                        height: MediaQuery.of(context).size.height,
-                        child: Image.asset(
-                          'assets/images/loading.jpg',
-                          fit: BoxFit.cover,
-                        )),
-                    Container(
-                      color: Colors.black54,
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                      height: 65,
-                      padding: const EdgeInsets.symmetric(horizontal: 5),
-                      child: Search(
-                        onPressed: (String name) async {
-                          await store.charactersFromName(name);
-                        },
-                        onChange: (value) {
-                          if (value == "") {
-                            store.fetchCharacters();
-                          }
-                        },
-                      ),
-                    ),
-    TripleBuilder(
-    store: store,
-    builder: (context, triple) {
-      return triple.isLoading?LinearProgressIndicator(color: Colors.red,backgroundColor: Colors.white,):Container();
-    }),
-
-                    Expanded(
-                      flex: 10,
-                      child: Stack(
-                        children: [
-                          ScopedBuilder(
-                            store: store,
-                            onLoading: (context) => Column(mainAxisAlignment: MainAxisAlignment.end, children: [initial]),
-                            onState: (context, value) => initial = Container(),
-                          ),
-                          TripleBuilder(
-                            store: store,
-                            builder: (context, triple) {
-                              return store.currentList.length != 0
-                                  ? ListView.builder(
-                                  controller: store.scrollController,
-                                  padding: const EdgeInsets.all(8),
-                                  itemCount: store.currentList.length,
-                                  itemBuilder: (BuildContext context, int index) {
-                                    var character = store.currentList[index];
-                                    if (index == store.currentList.length - 1) {
-                                      return Center(child: CircularProgressIndicator());
-                                    }
-                                    return CharactersList(
-                                      character: character,
-                                    );
-                                  })
-                                  : Center(
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    Container(
-                                      child: Text(
-                                        "Hero is not found",
-                                        style: TextStyle(color: Colors.white, fontSize: 25),
-                                      ),
+      children: [
+        Stack(
+          children: [
+            Container(
+                height: MediaQuery.of(context).size.height,
+                child: Image.asset(
+                  'assets/images/loading.jpg',
+                  fit: BoxFit.cover,
+                )),
+            Container(
+              color: Colors.black54,
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Container(
+              height: 65,
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: Search(
+                onPressed: (String name) async {
+                  await store.charactersFromName(name);
+                },
+                onChange: (value) {
+                  if (value == "") {
+                    store.fetchCharacters();
+                  }
+                },
+              ),
+            ),
+            TripleBuilder(
+                store: store,
+                builder: (context, triple) {
+                  return triple.isLoading
+                      ? LinearProgressIndicator(
+                          color: Colors.red,
+                          backgroundColor: Colors.white,
+                        )
+                      : Container();
+                }),
+            Expanded(
+              flex: 10,
+              child: Stack(
+                children: [
+                  ScopedBuilder(
+                    store: store,
+                    onLoading: (context) => Column(mainAxisAlignment: MainAxisAlignment.end, children: [initial]),
+                    onState: (context, value) => initial = Container(),
+                  ),
+                  TripleBuilder(
+                    store: store,
+                    builder: (context, triple) {
+                      return store.currentList.length != 0
+                          ? ListView.builder(
+                              controller: store.scrollController,
+                              padding: const EdgeInsets.all(8),
+                              itemCount: store.currentList.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                var character = store.currentList[index];
+                                // if (index == store.currentList.length - 1) {
+                                //   return Center(child: CircularProgressIndicator());
+                                // }
+                                return CharactersList(
+                                  character: character,
+                                );
+                              })
+                          : Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Container(
+                                    child: Text(
+                                      "Hero is not found",
+                                      style: TextStyle(color: Colors.white, fontSize: 25),
                                     ),
-                                    SizedBox(
-                                      height: 100,
-                                    )
-                                  ],
-                                ),
-                              );
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-
-              ],
-            )));
+                                  ),
+                                  SizedBox(
+                                    height: 100,
+                                  )
+                                ],
+                              ),
+                            );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    )));
   }
 }
